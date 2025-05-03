3650d7195d2e8610e620969ad211d0091bc03b92
KogarkoP <kogarkopavel@gmail.com>
2025-05-04 00:12:55 +0300

    Added product page styles

--- product/product.js
+++ product/product.js
@@ -23,8 +23,9 @@ const deleteProductById = async () => {
 const productName = document.getElementById("product_name");
 const productImgWrap = document.getElementById("product_img_wrapper");
 const productData = document.getElementById("product_data");
-const deleteBtn = document.getElementById("delete_btn");
-const message = document.getElementById("message");
+const message = document.createElement("h3");
+const deleteBtn = document.createElement("button");
+const backToCatalogBtn = document.createElement("button");
 
 const insertProductData = (product) => {
   document.title = `Product: ${product.name}`;
@@ -37,7 +38,10 @@ const insertProductData = (product) => {
 
   const productPrice = document.createElement("p");
   productPrice.classList.add("product_price");
-  productPrice.textContent = `${product.price} \u20AC with TAX`;
+  const withTax = document.createElement("span");
+  withTax.classList.add("with_tax");
+  withTax.textContent = "with TAX";
+  productPrice.textContent = `${product.price} \u20AC`;
 
   const productLocation = document.createElement("p");
   productLocation.classList.add("product_location");
@@ -47,10 +51,21 @@ const insertProductData = (product) => {
   productDescription.classList.add("product_description");
   productDescription.textContent = product.description;
 
+  message.classList.add("message");
+
+  deleteBtn.classList.add("delete_btn");
+  deleteBtn.textContent = "Delete product";
+
+  backToCatalogBtn.textContent = "Back to the catalog";
+
   productImgWrap.append(productImg);
   productData.append(productPrice);
+  productPrice.append(withTax);
   productData.append(productLocation);
   productData.append(productDescription);
+  productData.append(message);
+  productData.append(deleteBtn);
+  productData.append(backToCatalogBtn);
 };
 
 const buildScreen = async () => {
@@ -67,4 +82,8 @@ deleteBtn.addEventListener("click", async () => {
     message.classList.add("success");
     message.textContent = "Product was deleted from catalog";
   }
+});
+
+backToCatalogBtn.addEventListener("click", () => {
+  window.location.replace("../index.html");
 });
